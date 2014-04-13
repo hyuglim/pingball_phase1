0485aba2a6fb902502c9b6a8adba65e6a2e61123
Harlin Lee <harlin@mit.edu>
2014-04-13 13:02:07 -0400

    ball constructor method also added TODO: pass in the action gadget

--- src/phase1/Ball.java
+++ src/phase1/Ball.java
@@ -1,10 +1,16 @@
 package phase1;
 import java.lang.Float;
 
+import physics.*;
+
 public class Ball {
-	
+    public Vect velocity;
+    public Circle circle;
+    public final String name;
+    
 	public Ball (String name, Float x, Float y, Float xVel, Float yVel) {
-		
+	    this.name = name;
+	    this.circle = new Circle(x, y, 0.5);
+	    this.velocity = new Vect(xVel, yVel);
 	}
-	
 }
--- src/phase1/Gadget.java
+++ src/phase1/Gadget.java
@@ -4,4 +4,9 @@ import physics.*;
 public interface Gadget {    
     //public void trigger();
     //public void action();
+    //everyone's action method will be something like
+    //1. do the collision/reflection
+    //1. while doing this gadget's action ( e.g. use reflectrotatingwall when using flipper)
+    //2. call the next gadget.action() if it's not null
+    //recursive call!
 }
