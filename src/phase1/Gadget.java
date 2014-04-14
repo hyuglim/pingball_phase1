85d508b3bef45549d8ffdf6f23c20c4cb0da82d2
Harlin Lee <harlin@mit.edu>
2014-04-14 01:01:34 -0400

    added specs for ball and gadget adt

--- src/phase1/Gadget.java
+++ src/phase1/Gadget.java
@@ -1,13 +1,25 @@
 package phase1;
-import physics.*;
+//import physics.*;
 
-public interface Gadget {    
+/**
+ * @author Harlin
+ * An abstract data type to represent gadgets in a pingball board.
+ */
+public interface Gadget {
+    /**
+     * Add trigger-action hook to a gadget.
+     * @param gadget the gadget whose action will be hooked
+     */
     public void addTrigger(Gadget gadget);
+    
+    /**
+     * Checks whether the ball will collide and updates ball's velocity accordingly if there is a collision.
+     * @param ball the ball which might collide with the gadget
+     */
     public void collide(Ball ball);
+    
+    /**
+     * The gadget's action that can be triggered. It's specific to the type of each gadget.
+     */
     public void action();
-    //everyone's action method will be something like
-    //1. do the collision/reflection
-    //1. while doing this gadget's action ( e.g. use reflectrotatingwall when using flipper)
-    //2. call the next gadget.action() if it's not null
-    //recursive call!
 }
