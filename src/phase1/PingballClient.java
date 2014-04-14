7a2de5a95ce270b317b0a2147ec960c47d6f3b20
Dong Hyug Lim <dhlim@mit.edu>
2014-04-14 17:02:12 -0400

    interim server/client

--- src/phase1/PingballClient.java
+++ src/phase1/PingballClient.java
@@ -11,169 +11,44 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Queue;
-
-import physics.Geometry;
-import physics.Geometry.DoublePair;
+import java.util.concurrent.ConcurrentHashMap;
 
 
-//each client has a board
+// each client has two threads within it
+// The first thread communicates with the server
+// The second thread plays the game.
+// The client will have a shared memory that gives information about
+// which ball hit which wall.
 
-//main method takes file argument and makes a board from it.
-//optionally take host and port, which means it should connect to the server
-//and the server will 
 
-//we need a helper method for reading a board file(.pb) 
-//and initializing a Board object from that
-public class PingballClient implements Runnable{
-	
-	private Socket client = null;
-	private int id;
+public class PingballClient{
+
 	private boolean isSinglePlayerMode = true;
-	
-	private Board myBoard = null;
-	
+	// key = ball, value = "invisible"|"visible". store whether the ball hit an invisible wall.
+	private ConcurrentHashMap<Ball, String> hitInvisibles = new ConcurrentHashMap<>();
+	private GamePlayer player;
+	private Communicator messenger;
+
+	// -1: no wall hit, 0: top wall hit,
+	// 1: bottom wall hit, 2: right wall hit
+	private int whichWallHit = -1;
+
 
 	//multiplayer constructor
 	PingballClient(Socket socket, Board board) {
-		this.client = socket;
-		this.myBoard = board;
-		
+		this.player = new GamePlayer(board);
+		this.messenger = new Communicator(socket, board);
+		new Thread(player).start();
+		new Thread(messenger).start();
 	}
-	
+
 	//singleplayer constructor
 	PingballClient(Board board) {
-		this.myBoard = board;
-	}
+		
+	}	
 	
-	
-	@Override
-	public void run() {
-		if (isSinglePlayerMode) {
-			singleRun();
-		} else {
-			// helper method -> call handleConnection and handleRequest
-			multiRun();
-		}		
-	}
-	
-	private void singleRun() {
-		//TODO: do things here
-	}
-	
-	private void multiRun() {
-		try{
-			//if connection successful
-			handleConnection(client);			
-			client.close();
-			
-		} catch (IOException e) {
-			System.out.println("in or out failed");
-			System.exit(-1);
-		}
-	}
-	
-	
-	
-	/**
-     * Handle a single client connection. Returns when client disconnects.
-     * 
-     * @param socket socket where the client is connected
-     * @throws IOException if connection has an error or terminates unexpectedly
-     */
-    private void handleConnection(Socket socket) throws IOException {
-        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
 
-        try{
-        	
-        	String hello = "Welcome to Pingball. Let the board just sit there, and you can stare at the board";
-        	out.println(hello);        	       	
-        	
-            for (String line = in.readLine(); line != null; line = in.readLine()) { //reading client lines
-                String output = handleRequest(line);
-                if (output != null) {
-                	// receiving messages from the server to the client
-                	String[] tokens = output.split(" ");
-                	
-                	//CONFIRMING IF BALL HIT AN INVISIBLE WALL
-                	if(tokens[0].equals("invisible")){
-                		// sample input: invisible NAME
-                		String ballToBeRemoved = tokens[1];
-                		List<Ball> balls = myBoard.getBalls();
-                		
-                		for (Ball b : balls) {
-                			if (b.name.equals(ballToBeRemoved)) {
-                				balls.remove(b);
-                			}
-                		}                		
-                	}
-                	if(tokens[0].equals("visible")) {
-                		// DON'T DO ANYTHING. BUSINESS AS USUAL            		                		
-                	}
-                	if(tokens[0].equals("newBall")) {
-                		double x = Double.parseDouble(tokens[1]);
-                		double y = Double.parseDouble(tokens[2]);
-                		// TODO: ADD A NEW BALL AT X,Y LOC IN THE CLIENT                		
-                	}                	
-                	                    
-                }
-            }
-        } finally {
-            out.close();
-            in.close();
-        }
-    }
-    
-    /**
-     * Handler for client input, performing requested operations and returning an output message.
-     * 
-     * @param input message from client
-     * @return message to client
-     */
-    private String handleRequest(String input) {
-        
-        String[] tokens = input.split(" ");
-        
-        //goes into the Server file
-//        if (tokens[0].equals("h")) {
-//        	String[] firstArg = tokens[1].split("_");
-//        	String leftBoard = firstArg[0];
-//        	String[] secondArg = tokens[2].split("_");
-//        	String rightBoard = secondArg[0];
-//        	
-//        	
-//        }
-//        if (tokens[0].equals("v")) {
-//        	
-//        }
-        
-        if (tokens[0].equals("hit")) {
-        	double x = Double.parseDouble(tokens[1]);
-        	double y = Double.parseDouble(tokens[2]);
-        	
-        	// TODO: 
-        	// first, find the adjacent neighbor
-        	// second, see if the wall that is hit is invisible
-        	// third, if so, change the coordinate and send message to the adjacent neighbor
-        	
-        	
-        	
-            Geometry.DoublePair loc = new Geometry.DoublePair(x, y);
-        	
-        } 
-        // Should never get here--make sure to return in each of the valid cases above.
-        throw new UnsupportedOperationException();
-    }
-	
-	public int getId() {
-		return id;
-	}
-	
-	//called by Server to assign clients id
-	public void setId(int id) {
-		this.id = id;
-	}
-    
+
 	public boolean isSinglePlayerMode() {
 		return isSinglePlayerMode;
 	}
@@ -181,7 +56,7 @@ public class PingballClient implements Runnable{
 	public void setSinglePlayerMode(boolean isSinglePlayerMode) {
 		this.isSinglePlayerMode = isSinglePlayerMode;
 	}
-	
+
 	public static void main(String[] args) {
 		int port = 10987; // default port
 		String host = ""; // if no host provided, go to single player mode
@@ -213,47 +88,46 @@ public class PingballClient implements Runnable{
 					throw new IllegalArgumentException("unable to parse number for " + flag);
 				}
 			}
-			
+
 		} catch (IllegalArgumentException iae) {
 			iae.printStackTrace();
 			System.err.println(iae.getMessage());
 			System.err.println("usage: PingballClient [--host HOST] [--port PORT] FILE");
 			return;
 		}
-		
+
 		System.out.println("port: " + port);
 		System.out.println("host: " + host);
 		System.out.println("file: " + file);
-		
-		
-		
+
+
+
 		//starting threads and shit
-		
+
 		//single player mode
 		if (host.equals("")) {
 			try {
 				System.out.println("single player");
 				Board board = new Board(file);
 				PingballClient player = new PingballClient(board);
-				player.setSinglePlayerMode(true);
-				new Thread(player).start();
+				
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
-			
+
 		} else { //multiplayer mode
 			try {
 				System.out.println("multiplayer");
 				Board board = new Board(file);
-				Socket socket = new Socket("localhost", port);
+				Socket socket = new Socket(host, port);
 				PingballClient player = new PingballClient(socket, board);
-				new Thread(player).start();
+				
 			} catch (Exception e) {
 				e.printStackTrace();			
 			}
 		}				
 
-		
+
 	}
-    
+
 }
--- src/phase1/PingballServer.java
+++ src/phase1/PingballServer.java
@@ -1,26 +1,30 @@
 package phase1;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Arrays;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Queue;
 import java.util.concurrent.atomic.AtomicInteger;
-
+import physics.*;
 
 public class PingballServer {
-    
+
 	private final ServerSocket serverSocket;
 	//private AtomicInteger clientId = new AtomicInteger(0);
-	
-	
+
+
 	public PingballServer(int port) throws IOException {
 		this.serverSocket = new ServerSocket(port);
 	}
-	
+
 	/**
 	 * Run the server, listening for client connections and handling them.
 	 * Never returns unless an exception is thrown.
@@ -31,27 +35,89 @@ public class PingballServer {
 	public void serve() throws IOException {
 
 		while (true) {
-			Socket socket = null;
+			final Socket socket;
 			try {
 				// block until a client connects
 				socket = serverSocket.accept();
-				
-				 				
+				new Thread(){
+					public void run(){
+						try {
+							handleConnection(socket);
+						} catch (IOException e) {
+							System.out.println("handleConnection failed inside Server");
+							e.printStackTrace();
+						}
+					}
+				}.start();
+
 			} catch (Exception e) {
 				System.out.println("client connection failed");
 			} 
 		}		
 	}
-	
-	
-	
-    //Main method checks for port argument and handles connection
-    public static void main(String[] args) {
-    	int port = 10987; // default port
+
+	private void handleConnection(Socket socket) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+		PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+
+		try{      	       	
+
+			for (String line = in.readLine(); line != null; line = in.readLine()) { //reading client lines
+				String output = handleRequest(line); 
+				if (output != null) {
+					
+				}
+			}
+		} finally {
+			out.close();
+			in.close();
+		}
+	}
+
+	private String handleRequest(String input) {
+
+		String[] tokens = input.split(" ");
+
+		//goes into the Server file
+		if (tokens[0].equals("h")) {
+			String[] firstArg = tokens[1].split("_");
+			String leftBoard = firstArg[0];
+			String[] secondArg = tokens[2].split("_");
+			String rightBoard = secondArg[0];
+
+
+		}
+		if (tokens[0].equals("v")) {
+
+		}
+
+		if (tokens[0].equals("hit")) {
+			double x = Double.parseDouble(tokens[1]);
+			double y = Double.parseDouble(tokens[2]);
+
+			// TODO: 
+				// first, find the adjacent neighbor
+			// second, see if the wall that is hit is invisible
+			// third, if so, change the coordinate and send message to the adjacent neighbor
+
+
+
+			Geometry.DoublePair loc = new Geometry.DoublePair(x, y);
+
+		} 
+
+		// Should never get here--make sure to return in each of the valid cases above.
+		throw new UnsupportedOperationException();
+	}
+
+
+	//Main method checks for port argument and handles connection
+	public static void main(String[] args) {
+		int port = 10987; // default port
 
 		Queue<String> arguments = new LinkedList<String>(Arrays.asList(args));
-    	
-    	try {
+
+		try {
 			//command line parsing
 			while ( ! arguments.isEmpty()) {
 				String flag = arguments.remove();
@@ -60,31 +126,31 @@ public class PingballServer {
 						port = Integer.parseInt(arguments.remove());
 						System.out.println("port: " + port);
 					}
-					
+
 				} catch (Exception e) {
 					e.printStackTrace();
 					throw new IllegalArgumentException();
 				}
 			}
-			
+
 		} catch (IllegalArgumentException iae) {
 			iae.printStackTrace();
 			System.err.println(iae.getMessage());
 			System.err.println("usage: PingballClient [--host HOST] [--port PORT] FILE");
 			return;
 		}
-    	
-    	try {
+
+		try {
 			runPingballServer(port);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-    
-    public static void runPingballServer(int port) throws IOException{
-    	PingballServer s = new PingballServer(port);
-    	s.serve();
-    }
-    //in serve, listen for a connection 
-    
+
+	public static void runPingballServer(int port) throws IOException{
+		PingballServer s = new PingballServer(port);
+		s.serve();
+	}
+	//in serve, listen for a connection 
+
 }
